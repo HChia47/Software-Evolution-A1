842f9c6019b5a33d8aee8c9f7b77abccb8f28149
HChia47 <34344440+HChia47@users.noreply.github.com>
2021-02-24 18:15:53 +0100

    Changed tokenizing method to remove symbols, added stopwords removal, added allReqTokenized Sentences method

--- entry_script.py
+++ entry_script.py
@@ -4,7 +4,10 @@ import nltk
 import pandas as pd
 import numpy as np
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
 
 
 def write_output_file():
@@ -57,33 +60,77 @@ if __name__ == "__main__":
     
     def getInputLowRequirements():
         df = pd.read_csv("/input/low.csv")
-        return df
+        dfnp = df.to_numpy()
+        return dfnp
 
     def getInputHighRequirements():
         df = pd.read_csv("/input/high.csv")
-        return df
+        dfnp = df.to_numpy()
+        return dfnp
     
     # Tokenize a sentence and remove commas and dots
-    def tokenizeSentence(sentence):
-        tokens = word_tokenize(sentence)
+    # def tokenizeSentence(sentence):
+    #     tokens = word_tokenize(sentence)
+    #     i = 0
+    #     while i < len(tokens):
+    #         if tokens[i] == '.':
+    #             tokens.remove('.')
+    #         elif tokens[i] ==  ',':
+    #             tokens.remove(',')
+    #         elif tokens[i] == '\'':
+    #             tokens.remove('\'')
+    #         elif tokens[i] == 's':
+    #             tokens.remove('s')  
+    #         else:
+    #             stop_words = set(stopwords.words('english'))
+    #             if tokens[i] in stop_words:
+    #                 tokens.remove(tokens[i])
+    #             else:
+    #                 i+=1   
+    #     return tokens
+
+    # Tokenize a sentence and remove commas and dots
+    def tokenizeSentenceRegex(sentence):
+        tokenizer = RegexpTokenizer(r'\w+')
+        tokens = tokenizer.tokenize(sentence)
         i = 0
         while i < len(tokens):
-            if tokens[i] == '.':
-                tokens.remove('.')
-            elif tokens[i] ==  ',':
-                tokens.remove(',')   
+            stop_words = set(stopwords.words('english'))
+            if tokens[i] in stop_words:
+                tokens.remove(tokens[i])
             else:
-                i+=1
+                i+=1   
         return tokens
 
-    
-    dfLow = getInputLowRequirements()
-    npLow = dfLow.to_numpy()
-    print(npLow)
-    print(npLow[0,0])
-    print(npLow[0, :])
-    print(word_tokenize(npLow[0,1]))
-    tokensLow1 = tokenizeSentence(npLow[0,1])
-    print(tokensLow1)
-    print(type(tokensLow1))
+    def tokenizeAllRequirements(data):
+        listReqAndTokens = []
+        print("tokenizeAllReq")
+        allReqIDs = data[:, 0]
+        print("allReqIDs")
+        print(allReqIDs)
+        allReqSentences = data[:, 1]
+        print("allReqSentences")
+        print(allReqSentences)
+        allReqTokenizedSentences = []
+        for x in allReqSentences:
+            allReqTokenizedSentences.append(tokenizeSentenceRegex(x))
+        
+        print("allReqTokenizedSentences")
+        print(allReqTokenizedSentences)
+
+        for ID, Sentence in zip(allReqIDs, allReqTokenizedSentences):
+            tempList = [ID, Sentence]
+            listReqAndTokens.append(tempList)
+
+        print("listReqAndTokens")    
+        print(listReqAndTokens)
+        print(type(listReqAndTokens))
+        return listReqAndTokens
+
+    dataLow = getInputLowRequirements()
+    dataHigh = getInputHighRequirements()
+
+    tokenizeDataLow = tokenizeAllRequirements(dataLow)
+    tokenizeDataHigh = tokenizeAllRequirements(dataHigh)
+
     write_output_file()
